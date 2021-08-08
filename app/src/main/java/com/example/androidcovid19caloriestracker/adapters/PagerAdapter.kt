c4c271034e22a421817f46e4db138d8bd771f564
evans <ekibet@onfonmedia.com>
2021-08-08 23:42:04 +0300

    Meal Per day resprents by tablayout

---
+++ app/src/main/java/com/example/androidcovid19caloriestracker/adapters/PagerAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.androidcovid19caloriestracker.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.androidcovid19caloriestracker.view.fragments.DailyMealFragment
+
+
+class PagerAdapter(fragment: Fragment) : FragmentStateAdapter (fragment) {
+    override fun getItemCount(): Int  = 7
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> DailyMealFragment()
+            1 -> DailyMealFragment()
+            2 -> DailyMealFragment()
+            3 -> DailyMealFragment()
+            4 -> DailyMealFragment()
+            5 -> DailyMealFragment()
+            6 -> DailyMealFragment()
+            else -> DailyMealFragment()
+        }
+    }
+
+}
--- app/src/main/java/com/example/androidcovid19caloriestracker/view/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/androidcovid19caloriestracker/view/fragments/HomeFragment.kt
@@ -1,25 +1,54 @@
 package com.example.androidcovid19caloriestracker.view.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.androidcovid19caloriestracker.R
+import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
+import com.example.androidcovid19caloriestracker.adapters.PagerAdapter
 import com.example.androidcovid19caloriestracker.databinding.FragmentHomeBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class HomeFragment : Fragment() {
 
     lateinit var binding: FragmentHomeBinding
+    private lateinit var pagerAdapter: PagerAdapter
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         binding = FragmentHomeBinding.inflate(inflater)
+
+        activity?.onBackPressedDispatcher?.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                activity?.finish()
+            }
+        })
 
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        pagerAdapter = PagerAdapter(this)
+        binding.pager.adapter = pagerAdapter
+        TabLayoutMediator(binding.tabLayout, binding.pager) { tab, position ->
+            tab.text = when (position) {
+                0 -> "Monday"
+                1 -> "Tuesday"
+                2 -> "Wednesday"
+                3 -> "Thursday"
+                4 -> "Friday"
+                5 -> "Saturday"
+                6 -> "Sunday"
+                else -> "NULL"
+            }
+        }.attach()
+    }
+
 
 
 }
