2df5a6cce60a0642747790a9557d75ce2dfd16fc
evans <ekibet@onfonmedia.com>
2021-08-17 19:37:46 +0300

    fixes to the set the splashtimeout logic in view model

--- app/src/main/java/com/example/androidcovid19caloriestracker/view/fragments/SplashFragment.kt
+++ app/src/main/java/com/example/androidcovid19caloriestracker/view/fragments/SplashFragment.kt
@@ -6,12 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
 import com.example.androidcovid19caloriestracker.R
 import com.example.androidcovid19caloriestracker.databinding.FragmentSplashBinding
+import com.example.androidcovid19caloriestracker.viewmodel.SplashViewModel
 
 class SplashFragment : Fragment() {
     lateinit var binding: FragmentSplashBinding
+    private lateinit var splashViewModel: SplashViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,9 +22,15 @@ class SplashFragment : Fragment() {
     ): View {
         binding = FragmentSplashBinding.inflate(inflater)
 
-        Handler().postDelayed({
-            findNavController().navigate(R.id.home2Fragment, null)
-        }, 2000)
+        splashViewModel = SplashViewModel(requireContext())
+
+        splashViewModel.getSplashTimeout()?.observe(requireActivity(), Observer {
+            if(it){
+                findNavController().navigate(R.id.home2Fragment, null)
+            }
+        })
+
+        splashViewModel.setSplashTimeout(true)
 
         return binding.root
     }
--- app/src/main/java/com/example/androidcovid19caloriestracker/viewmodel/SplashViewModel.kt
+++ app/src/main/java/com/example/androidcovid19caloriestracker/viewmodel/SplashViewModel.kt
@@ -1,7 +1,28 @@
 package com.example.androidcovid19caloriestracker.viewmodel
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SplashViewModel: ViewModel() {
+class SplashViewModel(
+    @SuppressLint("StaticFieldLeak") val context: Context
+): ViewModel() {
 
+    private var ready: MutableLiveData<Boolean>? = null
+
+    fun setSplashTimeout(value: Boolean){
+        Handler(Looper.myLooper()!!).postDelayed({
+            ready?.value = value
+        }, 2000)
+    }
+    
+    fun getSplashTimeout(): MutableLiveData<Boolean>?{
+        if(ready == null){
+            ready = MutableLiveData()
+        }
+        return ready
+    }
 }
